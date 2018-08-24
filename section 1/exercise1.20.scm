15cceff4f8d6f223fdf560238e8b53b38ca68cfe
hankail05 <39765074+hankail05@users.noreply.github.com>
2018-08-24 23:16:58 +0900

    practice

--- section 1/exercise1.20.scm
+++ section 1/exercise1.20.scm
@@ -7,31 +7,31 @@
 
 ; In normal order
 ; (gcd 206 40)
-;
+;  ↓
 ; (if (= 40 0)
 ;     206
 ;     (gcd 40 (remainder 206 40)))
 ;
 ; (gcd 40 (remainder 206 40))
-;
+;  ↓
 ; (if (= (remainder 206 40) 0)
 ;     40
 ;     (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
 ;
 ; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
-;
+;  ↓
 ; (if (= (remainder 40 (remainder 206 40)) 0)
 ;     (remainder 206 40)
 ;     (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
 ;
 ; (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
-;
+;  ↓
 ; (if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)
 ;     (remainder 40 (remainder 206 40))
 ;     (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
 ;
 ; (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
-;
+;  ↓
 ; (if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0)
 ;     (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
 ;     ...) <- not evaluated
@@ -40,31 +40,31 @@
 ;
 ; In applicative order
 ; (gcd 206 40)
-;
+;  ↓
 ; (if (= 40 0)
 ;     206
 ;     (gcd 40 (remainder 206 40)))
 ;
 ; (gcd 40 6)
-;
+;  ↓
 ; (if (= 6 0)
 ;     40
 ;     (gcd 6 (remainder 40 6)))
 ;
 ; (gcd 6 4)
-;
+;  ↓
 ; (if (= 4 0)
 ;     6
 ;     (gcd 4 (remainder 6 4)))
 ;
 ; (gcd 4 2)
-;
+;  ↓
 ; (if (= 2 0)
 ;     4
 ;     (gcd 2 (remainder 4 2)))
 ;
 ; (gcd 2 0)
-;
+;  ↓
 ; (if (= 0 0)
 ;     2
 ;     (gcd 0 (remainder 2 0))) <= not evaluated
