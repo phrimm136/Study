c7cef7ae1c24981140953e9b4546701af4c3607a
hankail05 <39765074+hankail05@users.noreply.github.com>
2018-10-14 23:01:42 +0900

    practice

---
+++ Section 3/exercise 3.21.scm
@@ -0,0 +1,40 @@
+#lang sicp
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" (print-queue queue))
+      (car (front-ptr queue))))
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue) (set-front-ptr! queue new-pair) (set-rear-ptr! queue new-pair) (print-queue queue))
+          (else (set-cdr! (rear-ptr queue) new-pair) (set-rear-ptr! queue new-pair) (print-queue queue)))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue) (error "DELETE! called with an empty queue" (print-queue queue)))
+        (else (set-front-ptr! queue (cdr (front-ptr queue))) (print-queue queue))))
+(define (print-queue queue)
+  (front-ptr queue))
+
+
+(define q1 (make-queue))
+; (insert-queue! q1 'a) -.> ((a) a)
+; (insert-queue! q1 'b) -> ((a b) b)
+; (delete-queue! q1) -> ((b) b)
+; (delete-queue! q1) -> (() b)
+
+; All queue operations print a pair of front pointer (whole itmes) and rear poiner (the last item),
+; so it's clear that the last item is printed twice, but not inserted twice.
+; To print queue correctly, we only need to print front pointer of queue.
+
+; The print-queue procedure is described above.
+
+(insert-queue! q1 'a) ; (a)
+(insert-queue! q1 'b) ; (a b)
+(delete-queue! q1) ; (b)
+(delete-queue! q1) ; ()
+; good
