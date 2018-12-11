8d5eb64e414762327191260c56cfd6a4f8e13baf
hankail05 <39765074+hankail05@users.noreply.github.com>
2018-12-11 20:31:47 +0900

    practice

--- SICP/Section 5/exercise 5.22.scm
+++ SICP/Section 5/exercise 5.22.scm
@@ -1,4 +1,33 @@
 #lang sicp
+; a.
+
+(controller (assign continue (label append-end))
+
+            tokenize-loop
+            (test (op null?) (reg x))
+            (branch (label x-null))
+            (save continue)
+            (assign x-ele (op car) (reg x))
+            (assign x (op cdr) (reg x))
+            (save x-ele)
+            (assign continue (label append-loop))
+            (goto (label break-loop))
+
+            x-null
+            (assign z (reg y))
+            (goto (reg continue))
+
+            append-loop
+            (restore x-ele)
+            (assign z (op cons) (reg x-ele) (reg z))
+            (restore continue)
+            (goto (reg continue))
+
+            append-end)
+
+
+; b.
+
 (controller (assign tmp (reg x))
                   
             append-loop
