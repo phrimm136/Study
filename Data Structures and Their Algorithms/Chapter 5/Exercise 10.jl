fd9d31aae90289701ffda958d4708b3c7d45e0a8
hankail05 <39765074+hankail05@users.noreply.github.com>
2019-05-19 18:59:12 +0900

    incomplete

---
+++ Data Structures and Their Algorithms/Chapter 5/Exercise 10.jl
@@ -0,0 +1,114 @@
+#=
+
+Q.
+
+Solve Problem 9 with the additional requirement that tables are deallocated as soon as possible, still with constant-time Access and Assign. (You will have to use slightly more storage overhead.)
+
+A.
+
+=#
+
+function dealloc(M, addr)
+    for i in reverse(addr)
+        if length(i) == 1
+            if !isnothing(M[i[1]])
+                if all(map(isnothing, M[i[1]]))
+                    M[i[1]] = nothing
+                    filter!(x->x != i, M)
+                end
+            end
+        else
+            if !isnothing(M[i[1]][i[2]])
+                if all(map(isnothing, M[i[1]][i[2]]))
+                    M[i[1]][i[2]] = nothing
+                    filter!(x->x != i, M)
+                end
+            end
+        end
+    end
+end
+
+
+function initialize(M, addr, v)
+    for i = 1:length(M)
+        if !isnothing(M[i])
+            for j = 1:length(M[i])
+                if !isnothing(M[i][j])
+                    for k = 1:length(M[i][j])
+                        M[i][j][k] = v
+                    end
+                end
+            end
+        end
+    end
+    dealloc(M, addr)
+end
+
+
+function access(M, addr, i, j, k)
+    dealloc(M, addr)
+    return M[i][j][k]
+end
+
+
+function assign(M, addr, i, j, k, v)
+    M[i][j][k] = v
+    dealloc(M, addr)
+end
+
+
+function iterate(M, addr)
+    dealloc(M, addr)
+    for i = 1:length(M)
+        if !isnothing(M[i])
+            println(i, " ")
+            for j = 1:length(M[i])
+                if !isnothing(M[i][j])
+                    print(j, ": ")
+                    for k = 1:length(M[i][j])
+                        if (!isnothing(M[i][j][k]))
+                            print(M[i][j][k], " ")
+                        end
+                    end
+                    println("")
+                end
+            end
+        end
+    end
+end
+
+
+function newtable(M, addr, n, i=0, j=0)
+    if i == 0
+        resize!(arr, n)
+        fill!(M, nothing)
+    elseif j == 0
+        M[i] = Array{Any}(nothing, n)
+        dealloc(M, addr)
+        push!(addr, [i])
+    else
+        M[i][j] = Array{Any}(nothing, n)
+        dealloc(M, addr)
+        push!(addr, [i, j])
+    end
+end
+
+addr = []
+arr = []
+newtable(arr, addr, 5)
+
+newtable(arr, addr, 6, 1)
+newtable(arr, addr, 4, 1, 4)
+assign(arr, addr, 1, 4, 2, 10)
+
+newtable(arr, addr, 6, 4)
+newtable(arr, addr, 4, 4, 1)
+newtable(arr, addr, 4, 4, 2)
+assign(arr, addr, 4, 2, 3, 20)
+
+iterate(arr, addr)
+
+newtable(arr, addr, 4, 1, 2)
+initialize(arr, addr, 3)
+
+iterate(arr, addr)
---
+++ Data Structures and Their Algorithms/Chapter 5/Exercise 5.jl
@@ -0,0 +1,94 @@
+#=
+
+Q.
+
+a. Generalize Problem 4 to arrays with indices (0 .. n) x (O .. m).
+
+b. Further generalize Problem 4 to k-dimensional arrays with arbitrary index sets.
+
+A.
+
+a.
+
+=#
+
+function order_transform!(M)
+    # Change the order of M from row-first to column-first.
+    len = [length(M), length(M[1])]
+    if len[1] == len[2]
+        for i = 1:length(M)
+            for j = (i+1):length(M[i])
+                tmp = M[i][j]
+                M[i][j] = M[j][i]
+                M[j][i] = tmp
+            end
+        end
+    else
+        i = 1
+        i1 = 1
+        for j = 2:ceil(Int, len[2] / 2)
+            j1 = j
+            tmp2 = M[i][j]
+            while true
+                tmp1 = M[j][i]
+                M[j][i] = tmp2
+                tmp2 = tmp1
+                len_linear = (j-1) * len[2] + i
+                i = ceil(Int, len_linear / len[1])
+                j = (len_linear-1) % len[1] + 1
+                if (i == i1 && j == j1)
+                    break
+                end
+            end
+        end
+    end
+    return M
+end
+
+
+M = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24 ,25], [26, 27, 28, 29, 30]]
+order_transform!(M)
+M # Well done.
+
+
+#=
+
+b.
+
+postpone to implement later.
+
+=#
+
+
+function length_N(M)
+    arr = M
+    len = []
+    while isa(arr, Array)
+        push!(len, arr[1])
+        arr = arr[1]
+    end
+    return len
+end
+
+
+function order_transform_N!(M)
+    len = length_N(M)
+    i = 1
+    i1 = 1
+    for j = 2:ceil(Int, len[2] / 2)
+        j1 = j
+        tmp2 = M[i][j]
+        while true
+            tmp1 = M[j][i]
+            M[j][i] = tmp2
+            tmp2 = tmp1
+            len_linear = (j-1) * len[2] + i
+            i = ceil(Int, len_linear / len[1])
+            j = (len_linear-1) % len[1] + 1
+            if (i == i1 && j == j1)
+                break
+            end
+        end
+    end
+    return M
+end
