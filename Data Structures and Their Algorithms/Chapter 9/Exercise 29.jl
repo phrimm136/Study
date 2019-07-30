893afafcc32631bef518963766286bc287bea838
hankail05 <39765074+hankail05@users.noreply.github.com>
2019-07-30 20:36:24 +0900

    practice

---
+++ Data Structures and Their Algorithms/Chapter 9/Exercise 29.jl
@@ -0,0 +1,15 @@
+#=
+
+Q.
+
+Explain why the structure of a quad tree does not depend on the order
+in which items are inserted into it.
+
+A.
+
+When raeching a leaf, instead of passing through, the algorithm split
+its block into four and reallocate it and an element to insert,
+and the location of them is determined with their location value.
+So the quad-tree structure is independent to the order of insertions.
+
+=#
