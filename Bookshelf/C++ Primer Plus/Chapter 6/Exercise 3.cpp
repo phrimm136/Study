3a7b640679a1ae2ba275c45f99afe46a3fe7f3a7
hankail05 <39765074+hankail05@users.noreply.github.com>
2019-10-14 01:51:24 +0900

    practice

---
+++ Bookshelf/C++ Primer Plus/Chapter 6/Exercise 3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    char ch;
+
+    std::cout << "Please enter one of the following choices:\n"
+              << "c) carnivore\t\tp) pianist\n"
+              << "t) tree\t\t\tg) game\n";
+    std::cin >> ch;
+
+    while (ch != 'c' && ch != 'p' && ch != 't' && ch != 'g')
+    {
+        std::cout << "Please enter a c, p, t, or g: ";
+        std::cin >> ch;
+    }
+
+    switch (ch)
+    {
+    case 'c':
+        std::cout << "A lion is a carnivore.\n";
+        break;
+    case 'p':
+        std::cout << "Bach is a pianist.\n";
+        break;
+    case 't':
+        std::cout << "A maple is a tree.\n";
+        break;
+    case 'g':
+        std::cout << "Starcraft is a game.\n";
+        break;
+    }
+
+    return 0;
+}
---
+++ Bookshelf/C++ Primer Plus/Chapter 6/Exercise 4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+#define strsize 100
+
+struct bop
+{
+    char fullname[strsize]; // real name
+    char title[strsize];    // job title
+    char bopname[strsize];  // secet BOP name
+    int preference;         // 0 = fullname, 1 = title, 2 = bopname
+};
+
+int main(int argc, char *argv[])
+{
+    struct bop bop[5] =
+        {
+            {"Wimp Macho", "Undergraduate Student", "WISP", 0},
+            {"Raki Rhodes", "Junior Programmer", "RHINO", 1},
+            {"Celia Laiter", "Archietecture Designer", "MIPS", 2},
+            {"Hoppy Hipman", "Anaylyst Trainee", "PENNY", 1},
+            {"Pat Hand", "Chairman", "LOOPY", 2}
+        };
+    char ch;
+
+    std::cout << "Benevolent Order of Programmers Report\n"
+              << "a. display by name\tb. display by title\n"
+              << "c. display by bopname\td.display by preference\n"
+              << "q. quit\n";
+    std::cout << "Enter your choice: ";
+    while (true)
+    {
+        std::cin >> ch;
+
+        switch (ch)
+        {
+        case 'a':
+            for (int i = 0; i < 5; ++i)
+                std::cout << bop[i].fullname << " \n";
+            break;
+        case 'b':
+            for (int i = 0; i < 5; ++i)
+                std::cout << bop[i].title << " \n";
+            break;
+        case 'c':
+            for (int i = 0; i < 5; ++i)
+                std::cout << bop[i].bopname << " \n";
+            break;
+        case 'd':
+            for (int i = 0; i < 5; ++i)
+                switch (bop[i].preference)
+                {
+                case 0:
+                    std::cout << bop[i].fullname << " \n";
+                    break;
+                case 1:
+                    std::cout << bop[i].title << " \n";
+                    break;
+                case 2:
+                    std::cout << bop[i].bopname << " \n";
+                    break;
+                }
+            break;
+        case 'q':
+            std::cout << "Bye!\n";
+            return 0;
+        }
+        std::cout << "Next choice: ";
+    }
+}
